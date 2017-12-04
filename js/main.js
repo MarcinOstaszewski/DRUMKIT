868bb5bd11956cc669ea5a853ca4aeee155d1323
MarcinOstaszewski <marcinostaszewski@gmail.com>
2017-12-04 14:08:14 +0100

    add basic functionality

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.addEventListener('keydown', function(e) {
+        console.log(e.keyCode);
+        let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        if (!audio) return;
+        audio.currentTime = 0;
+        audio.play();
+    })
+
+});
